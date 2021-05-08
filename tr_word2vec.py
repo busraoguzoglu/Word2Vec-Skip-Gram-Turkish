91dd8c1097d52ab7228014b6ca079143659971ad
Büşra Oğuzoğlu <busra.oguzoglu@gmail.com>
2021-05-08 16:02:40 +0300

    Add files via upload

--- tr_word2vec.py
+++ tr_word2vec.py
@@ -1,4 +1,28 @@
 from gensim.models import Word2Vec
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def tsne(model):
+
+    # t-SNE Visualization:
+
+    vocab = list(model.wv.key_to_index)
+    X = model.wv[vocab]
+    tsne = TSNE(n_components=2)
+    X_tsne = tsne.fit_transform(X)
+    df = pd.DataFrame(X_tsne, index=vocab, columns=['x', 'y'])
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax.scatter(df['x'], df['y'])
+    for word, pos in df.iterrows():
+        ax.annotate(word, pos)
+
+def analogy(model, worda, wordb, wordc):
+    result = model.most_similar(negative=[worda],
+                                positive=[wordb, wordc])
+
+    return result[0][0]
 
 def main():
 
@@ -18,15 +42,27 @@ def main():
     sentences = [s.split() for s in sentences]
     print(len(sentences))
 
-    #model = Word2Vec(sentences=sentences, vector_size=100, window=5, min_count=5, workers=4)
-    #model.save("word2vec.model")
+    model = Word2Vec(sentences=sentences, vector_size=100, window=5, min_count=1, workers=5)
+    model.save("model/size100window5.model")
 
     # Test:
 
-    model = Word2Vec.load("word2vec.model")
+    model = Word2Vec.load("model/size100window5.model")
+    word_vectors = model.wv
 
     print('numpy vector:', model.wv['çok'])  # get numpy vector of a word
-    print('similars:', model.wv.most_similar('sen', topn=10))
+    print('similars:', model.wv.most_similar('git', topn=5))
+
+    # Evaluate Analogies
+
+    test1 = analogy(word_vectors, 'erkek', 'baba', 'kadın')
+    print(test1)
+    test1 = analogy(word_vectors, 'sen', 'siz', 'ben')
+    print(test1)
+    test1 = analogy(word_vectors, 'bir', 'birinci', 'üç')
+    print(test1)
+
+    # Evaluate Similarity
 
 if __name__ == '__main__':
     main()
